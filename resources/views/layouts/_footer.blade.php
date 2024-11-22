43b2106996a222628cb4812b3299ce511326d2f0
zhangshuai <1121323952@qq.com>
2024-11-22 17:05:17 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -8,7 +8,7 @@
     <a href="https://learnku.com/laravel/courses" target="_blank" class="text-decoration-none">刻意练习，每日精进</a>
 
     <div class="float-end">
-        <a href="/about" class="text-decoration-none">关于</a>
+        <a href="{{ route('about') }}" class="text-decoration-none">关于</a>
     </div>
     
 </footer>
