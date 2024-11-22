5687da946c401256c7a4fb53ac4623c27efd5b3d
zhangshuai <1121323952@qq.com>
2024-11-22 16:56:29 +0800

    切割头部和尾部子视图

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,23 +1,23 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
-    @vite(['resources/sass/app.scss','resources/js/app.js'])
-  </head>
-  <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container">
-        <a class="navbar-brand" href="/">Weibo App</a>
-        <ul class="navbar-nav justify-content-end">
-          <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-        </ul>
-      </div>
-    </nav>
+<head>
+  <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
+  @vite(['resources/sass/app.scss','resources/js/app.js'])
+</head>
 
-    <div class="container">
+<body>
+
+  @include('layouts._header')
+
+  <div class="container">
+    <div class="offset-md-1 col-md-10">
       @yield('content')
+      @include('layouts._footer')
     </div>
-  </body>
+  </div>
+
+
+</body>
+
 </html>
